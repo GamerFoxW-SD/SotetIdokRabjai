04118f3aff96b406eea21767378dfe1b1f640108
GamerFoxW-SD <gamerfoxwsd@gmail.com>
2025-04-07 00:02:02 +0200

    leirasok XD

--- csapat_list.js
+++ csapat_list.js
@@ -3,14 +3,14 @@ const csapatAdatok = [
       nev: "Dani",
       alias: "A Fekete Lovag",
       szak: "Zümi",
-      leiras: "",
+      leiras: "A Fekete Kovag \nEgykor középkori lovag volt, de egy titkos időkapun át a jelenbe került. Most informatikát tanul, és a kardját billentyűzetre cserélte. Harcmodora precíz, mint a kódja, és minden bug ellen úgy küzd, mintha sárkány lenne. A múlt árnya és a digitális jövő mestere egy személyben.",
       kep: "kep/lovag.png" // Kép elérési útja
     },
     {
       nev: "Laci",
       alias: "A Retro Reper",
       szak: "Zümi",
-      leiras: "",
+      leiras: "A Retro Reper \nA '80-as évek underground rapcsászára, aki breaktánccal nyitotta a csatákat, és kazettás magnóval szórta az igazságot. Egy hibás szintetizátor teleportálta a jelenbe, ahol most informatikusnak tanul. Flow-ja ma is penge, csak most már kódban pörög. A parancssorban beatboxol, a bugokat freestyle-ban győzi le.",
       kep: "kep/retro_reper.png" // Kép elérési útja
     },
    /* {
@@ -31,7 +31,7 @@ const csapatAdatok = [
         nev: "Ádám",
         alias: "Mélységjáró",
         szak: "Progi",
-        leiras: "A Mélységjáró A matróz, akit a tenger nevelt, és akinek szíve a mélységek ritmusára dobban. Titokzatos alak, ki a sötét hullámok között bújik meg, mindörökre a víz titkainak őrzője.\n",
+        leiras: "A Mélységjáró \nA matróz, akit a tenger nevelt, és akinek szíve a mélységek ritmusára dobban. Titokzatos alak, ki a sötét hullámok között bújik meg, mindörökre a víz titkainak őrzője.\n",
         kep: "kep/Melyseg.png" // Kép elérési útja
       }
   ];
